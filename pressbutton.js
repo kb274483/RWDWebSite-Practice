ef324283516a65bc538615504337015a308284ef
HaoChuanLee <kb274483@gmail.com>
2020-12-24 10:41:35 +0800

    Add files via upload

---
+++ pressbutton.js
@@ -0,0 +1,10 @@
+function press(elem){
+    let button = document.querySelector("#linesbutton");
+    console.log("滑鼠點下");
+    button.style.display="block";
+}
+function closebutton(elem){
+    let button2 = document.querySelector("#linesbutton");
+    console.log("滑鼠點下");
+    button2.style.display="none";
+}
